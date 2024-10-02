3e87ac894376cddf18cd14c266cb213d8d93edd2
Francis Li <mail@francisli.com>
2024-10-02 15:44:59 -0700

    Tweak tests

--- test/routes/root.test.js
+++ test/routes/root.test.js
@@ -1,12 +1,17 @@
-import { test } from 'node:test';
+import { describe, it } from 'node:test';
 import * as assert from 'node:assert';
+import { StatusCodes } from 'http-status-codes';
 import { build } from '../helper.js';
 
-test('/health', async (t) => {
-  const app = await build(t);
-
-  const res = await app.inject({
-    url: '/health'
+describe('/health', () => {
+  describe('GET /', () => {
+    it('returns succesfully if the server and db connection is alive', async (t) => {
+      const app = await build(t);
+      const res = await app.inject({
+        url: '/health'
+      });
+      assert.deepStrictEqual(res.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(JSON.parse(res.payload), { alive: true });
+    });
   });
-  assert.deepStrictEqual(JSON.parse(res.payload), { alive: true });
 });
