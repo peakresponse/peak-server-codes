222e6616821bb33edc99ca45852dd670f7b64858
Francis Li <mail@francisli.com>
2024-10-02 06:45:37 -0700

    More schema tweaks, import script

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -1,40 +1,40 @@
 -- CreateTable
-CREATE TABLE "ICD10CMChapter" (
+CREATE TABLE "icd10_cm_chapters" (
     "name" TEXT NOT NULL,
     "position" INTEGER NOT NULL,
     "desc" TEXT,
 
-    CONSTRAINT "ICD10CMChapter_pkey" PRIMARY KEY ("name")
+    CONSTRAINT "icd10_cm_chapters_pkey" PRIMARY KEY ("name")
 );
 
 -- CreateTable
-CREATE TABLE "ICD10CMSection" (
+CREATE TABLE "icd10_cm_sections" (
     "id" TEXT NOT NULL,
     "desc" TEXT,
-    "chapterId" TEXT NOT NULL,
+    "chapter_name" TEXT NOT NULL,
 
-    CONSTRAINT "ICD10CMSection_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "icd10_cm_sections_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "ICD10CMCode" (
+CREATE TABLE "icd10_cm_codes" (
     "name" TEXT NOT NULL,
     "desc" TEXT,
-    "sectionId" TEXT NOT NULL,
-    "parentId" TEXT,
+    "section_id" TEXT NOT NULL,
+    "parent_name" TEXT,
     "depth" INTEGER NOT NULL,
     "lft" INTEGER NOT NULL,
     "rgt" INTEGER,
 
-    CONSTRAINT "ICD10CMCode_pkey" PRIMARY KEY ("name")
+    CONSTRAINT "icd10_cm_codes_pkey" PRIMARY KEY ("name")
 );
 
 -- AddForeignKey
-ALTER TABLE "ICD10CMSection" ADD CONSTRAINT "ICD10CMSection_chapterId_fkey" FOREIGN KEY ("chapterId") REFERENCES "ICD10CMChapter"("name") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "icd10_cm_sections" ADD CONSTRAINT "icd10_cm_sections_chapter_name_fkey" FOREIGN KEY ("chapter_name") REFERENCES "icd10_cm_chapters"("name") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "ICD10CMCode" ADD CONSTRAINT "ICD10CMCode_sectionId_fkey" FOREIGN KEY ("sectionId") REFERENCES "ICD10CMSection"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "icd10_cm_codes" ADD CONSTRAINT "icd10_cm_codes_section_id_fkey" FOREIGN KEY ("section_id") REFERENCES "icd10_cm_sections"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "ICD10CMCode" ADD CONSTRAINT "ICD10CMCode_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "ICD10CMCode"("name") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "icd10_cm_codes" ADD CONSTRAINT "icd10_cm_codes_parent_name_fkey" FOREIGN KEY ("parent_name") REFERENCES "icd10_cm_codes"("name") ON DELETE SET NULL ON UPDATE CASCADE;
 
