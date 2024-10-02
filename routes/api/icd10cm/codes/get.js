a4228f3136e61b154611aad04886e0bd1db8229b
Francis Li <mail@francisli.com>
2024-10-02 13:24:18 -0700

    Add schema definitions

--- routes/api/icd10cm/codes/get.js
+++ routes/api/icd10cm/codes/get.js
@@ -1,5 +1,31 @@
+import { StatusCodes } from 'http-status-codes';
+
 export default async function (fastify, opts) {
-  fastify.get('/:name', async function (request, reply) {
+  fastify.get('/:name', {
+    schema: {
+      params: {
+        type: 'object',
+        properties: {
+          name: { type: 'string' }
+        }
+      },
+      response: {
+        [StatusCodes.OK]: {
+          type: 'object',
+          properties: {
+            name: { type: 'string' },
+            desc: { type: 'string' },
+            sectionId: { type: 'string' },
+            parentName: { type: 'string' },
+            depth: { type: 'integer' },
+            lft: { type: 'integer' },
+            rgt: { type: 'integer' }
+          }
+        }
+      }
+    }
+  },
+  async function (request, reply) {
     const { name } = request.params;
     const record = await fastify.prisma.icd10CMCode.findUnique({
       where: { name }
--- routes/api/icd10cm/codes/search.js
+++ routes/api/icd10cm/codes/search.js
@@ -1,5 +1,35 @@
+import { StatusCodes } from 'http-status-codes';
+
 export default async function (fastify, opts) {
-  fastify.get('', async function (request, reply) {
+  fastify.get('', {
+    schema: {
+      querystring: {
+        type: 'object',
+        properties: {
+          page: { type: 'integer' },
+          perPage: { type: 'integer' }
+        }
+      },
+      response: {
+        [StatusCodes.OK]: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              name: { type: 'string' },
+              desc: { type: 'string' },
+              sectionId: { type: 'string' },
+              parentName: { type: 'string' },
+              depth: { type: 'integer' },
+              lft: { type: 'integer' },
+              rgt: { type: 'integer' }
+            }
+          }
+        }
+      }
+    }
+  },
+  async function (request, reply) {
     const { page = '1', perPage = '25' } = request.query;
     const options = {
       page,
