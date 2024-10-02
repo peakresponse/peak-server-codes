186ea70d84b44c664d7ab0043139fe504ea70ada
Francis Li <mail@francisli.com>
2024-10-02 14:05:44 -0700

    Clean up route tests

--- routes/root.js
+++ routes/root.js
@@ -1,6 +1,6 @@
 export default async function (fastify, opts) {
   fastify.get('/health', async function (request, reply) {
     const result = await fastify.prisma.$queryRaw`SELECT 1`;
-    return reply.send(result?.[0]['?column?'] === 1);
+    return reply.send({ alive: result?.[0]['?column?'] === 1 });
   });
 }
